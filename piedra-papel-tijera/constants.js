0f1bf830bcb2025fe5bb17baf1da57e47b3160dc
maumaco <mauriciomargaritcornejo@gmail.com>
2023-08-26 17:13:59 -0300

    Se agrega la validación del nombre y las rondas (validateUserName, validateRounds)

--- piedra-papel-tijera/constants.js
+++ piedra-papel-tijera/constants.js
@@ -1,5 +1,18 @@
-const DEFAULT_ROUNDS = 5;
+const CONF_DEFAULT_ROUNDS = 5;
+const CONF_MAX_ROUNDS = 99;
+const CONF_MIN_ROUNDS = 1;
+const CONF_NAME_MAX_LENGTH = 32;
 
+const ERR_EVEN_ROUNDS = 'El número de rondas debe ser impar';
+const ERR_LONG_NAME = `Tu nombre no puede superar los ${CONF_NAME_MAX_LENGTH} caracteres`;
+const ERR_MAX_ROUNDS = `El número máximo de rondas es ${CONF_MAX_ROUNDS}`;
+const ERR_MIN_ROUNDS = `El número mínimo de rondas es ${CONF_MIN_ROUNDS}`;
+const ERR_NOT_DIGIT = 'Solo puedes ingresar dígitos';
+const ERR_VOID_NAME = 'Es necesario que ingreses un nombre';
+const ERR_VOID_ROUNDS = 'Es necesario que ingreses un número de rondas';
+
+const ID_MESSAGE_TEXT = 'message-text';
+const ID_NAME_INPUT = 'name-input';
 const ID_PLAY_BUTTON = 'play-button';
 const ID_RESET_BUTTON = 'reset-button';
 const ID_ROUNDS_INPUT = 'rounds-input';
--- piedra-papel-tijera/functions.js
+++ piedra-papel-tijera/functions.js
@@ -1,15 +1,119 @@
 function setDefaultConfiguration() {
+  setControlDisabled(document.getElementById(ID_ROUNDS_INPUT), true);
   setControlDisabled(document.getElementById(ID_PLAY_BUTTON), true);
   setControlDisabled(document.getElementById(ID_RESET_BUTTON), true);
-  setControlValue(document.getElementById(ID_ROUNDS_INPUT), DEFAULT_ROUNDS);
+  setControlValue(document.getElementById(ID_ROUNDS_INPUT), CONF_DEFAULT_ROUNDS);
 }
 
+
+
 function setControlDisabled(control, disabled) {
-  control.disabled = disabled;
+  if (control.disabled !== disabled) {
+    control.disabled = disabled;
+  }
 }
 
 function setControlValue(control, value) {
-  control.value = value;
+  if (control.value !== value) {
+    control.value = value;
+  }
+}
+
+
+
+function validateRounds(rounds) {
+  let error = searchRoundsError(rounds);
+
+  if (error) {
+    setMessage(error);
+    setControlDisabled(document.getElementById(ID_PLAY_BUTTON), true);
+    validRounds = false;
+  }
+  else {
+    setMessage('');
+    if (validUserName) {
+      setControlDisabled(document.getElementById(ID_PLAY_BUTTON), false);
+    }
+    validRounds = true;
+  }
+}
+  
+function validateUserName(userName) {
+  let error = searchUserNameError(userName);
+
+  if (error) {
+    setMessage(error);
+    setControlDisabled(document.getElementById(ID_ROUNDS_INPUT), true);
+    setControlDisabled(document.getElementById(ID_PLAY_BUTTON), true);
+    validUserName = false;
+  }
+  else {
+    setMessage('');
+    setControlDisabled(document.getElementById(ID_ROUNDS_INPUT), false);
+    if (validRounds) {
+      setControlDisabled(document.getElementById(ID_PLAY_BUTTON), false);
+    }
+    validUserName = true;
+  }
+}
+
+
+
+function searchRoundsError(rounds) {
+  rounds = rounds.trim();
+
+  if (rounds.length === 0) {
+    return ERR_VOID_ROUNDS;
+  }
+  else if (rounds.search(/[^0-9]/) > -1) {
+    return ERR_NOT_DIGIT;
+  }
+
+  rounds = Number(rounds);
+
+  if (rounds < CONF_MIN_ROUNDS) {
+    return ERR_MIN_ROUNDS;
+  }
+  else if (rounds > CONF_MAX_ROUNDS) {
+    return ERR_MAX_ROUNDS;
+  }
+  else if (rounds % 2 === 0) {
+    return ERR_EVEN_ROUNDS;
+  }
+}
+
+function searchUserNameError(userName) {
+  userName = userName.trim();
+
+  if (userName.length === 0) {
+    return ERR_VOID_NAME;
+  }
+  else if (userName.length > CONF_NAME_MAX_LENGTH) {
+    return ERR_LONG_NAME;
+  }
+}
+
+
+
+function setMessage(message) {
+  if (message) {
+    showMessage(message);
+  }
+  else {
+    deleteMessage();
+  }
+}
+    
+function deleteMessage() {
+  if (document.getElementById(ID_MESSAGE_TEXT).innerText !== '') {
+    document.getElementById(ID_MESSAGE_TEXT).innerText = '';
+  }
+}
+
+function showMessage(message) {
+  if (document.getElementById(ID_MESSAGE_TEXT).innerText !== message) {
+    document.getElementById(ID_MESSAGE_TEXT).innerText = message;
+  }
 }
 
 
